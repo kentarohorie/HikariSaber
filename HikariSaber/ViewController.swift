a88ec9ee79d5678e3cc64825b3bbec6c3060c48b
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-12-15 19:13:56 +0900

    Initial Commit

---
+++ HikariSaber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HikariSaber
+//
+//  Created by 堀江健太朗 on 2015/12/15.
+//  Copyright © 2015年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
